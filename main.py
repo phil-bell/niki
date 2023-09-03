04136417136589cf9ad6343cadf134eba8f999d7
Philip Bell <philhabell@gmail.com>
2023-09-03 19:26:57 +0100

    Allowing server url to be set by env

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import websocket
 
 import app.handlers as handlers
 
+SERVER_URL = os.environ.get("SERVER_KEY", "wss://octopus-app-74lmf.ondigitalocean.app")
 SERVER_KEY = os.environ.get("SERVER_KEY")
 SERVER_SECRET = os.environ.get("SERVER_SECRET")
 
@@ -13,7 +14,7 @@ def main():
     websocket.enableTrace(False)
     run(["qbittorrent-nox", "-d"])
     wsapp = websocket.WebSocketApp(
-        f"ws://host.docker.internal:8000/server/{SERVER_KEY}",
+        f"{SERVER_URL}/server/{SERVER_KEY}",
         header={"authentication": SERVER_SECRET},
         on_open=handlers.on_open,
         on_message=handlers.on_message,
