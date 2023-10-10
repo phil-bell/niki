607e61a4ad7d266c6be99ee8e37db69edecf2580
Philip Bell <philhabell@gmail.com>
2023-10-10 21:10:07 +0100

    merging all files into a single file

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
+import json
 import os
-from subprocess import run
+import threading
 import time
+from subprocess import run
 
 import websocket
-
-from app.handlers import on_close, on_message, on_open
+from qbittorrent import Client
 
 SERVER_URL = os.environ.get("SERVER_URL", "wss://starfish-app-hxdcr.ondigitalocean.app")
 SERVER_KEY = os.environ.get("SERVER_KEY", "")
 SERVER_SECRET = os.environ.get("SERVER_SECRET", "")
+QB_PASSWORD = os.environ.get("QB_PASSWORD", "adminadmin")
+
+
+def qb_client() -> Client:
+    qb = Client("http://0.0.0.0:8080/")
+    qb.login("admin", QB_PASSWORD)
+    return qb
+
+
+def torrent_add(content: dict) -> str:
+    qb = qb_client()
+    message = qb.download_from_link(
+        content.get("magnet"),
+        savepath=content.get("location"),
+    )
+    print(f"Torrent added to: {content.get('location')}")
+    return message
+
+
+def router(message: str) -> str | None:
+    data = json.loads(message)
+    match data.get("type"):
+        case "torrent.add":
+            return torrent_add(content=data.get("content"))
+
+
+def on_open(ws) -> None:
+    print("Connection open")
+
+
+def on_message(ws, message: str) -> None:
+    threading.Thread(target=router, kwargs={"message": message}).start()
+
+
+def on_close(ws, close_status_code, close_msg) -> None:
+    print("Connection closed")
+    if close_status_code or close_msg:
+        print(f"{close_status_code}: {close_msg}")
 
 
 def main():
@@ -26,6 +65,7 @@ def main():
         print("Sleeping...")
         time.sleep(5)
         print("Restarting")
+
 
 if __name__ == "__main__":
     main()
